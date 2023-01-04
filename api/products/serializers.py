4d40598b3dc09901dc0da21952d390e8773c99c3
alejandroBarraza <alejandro.barraza@alumnos.ucn.cl>
2023-01-05 03:41:14 +0900

    changed to generics views

--- api/products/serializers.py
+++ api/products/serializers.py
@@ -1,9 +1,30 @@
 from rest_framework import serializers
-from products.models import Product
-
+from products.models import Product, User
+import random
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = [
+            'id',
+            'title',
+            'image',
+            'likes'
+        ]
+        ReadOnlyFields = ['id']
+
+    
+class UserSerializer(serializers.ModelSerializer):
+    random_user = serializers.SerializerMethodField()
+    class Meta:
+        model = User
+        fields = [
+            'random_user',
+        ]
+        ReadOnlyFields = ['random_user']
 
 
+    def get_random_user(self, obj):
+        users = User.objects.all()
+        user = random.choice(users)
+        return user.id
+ 
