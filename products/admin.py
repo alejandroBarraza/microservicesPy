9dc0e588d58e7995c7d10a7ddf8c8b6759625dda
alejandroBarraza <alejandro.barraza@alumnos.ucn.cl>
2023-01-05 03:49:36 +0900

    registered product and user

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,6 @@
-from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Product, User
+admin.site.register(Product)
+admin.site.register(User)
