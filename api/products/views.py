ab6a4fde8b177fe493681ce1e7e6294dcf8d3bfb
alejandroBarraza <alejandro.barraza@alumnos.ucn.cl>
2023-01-05 03:50:34 +0900

    modified view with generic api

--- api/products/views.py
+++ api/products/views.py
@@ -1,45 +1,32 @@
-from rest_framework import viewsets, status
+from rest_framework import generics
+from rest_framework.views import APIView
+from products.models import Product, User
+from .serializers import ProductSerializer, UserSerializer
+# from django.http import Http404
 from rest_framework.response import Response
-from products.models import Product
-from .serializers import ProductSerializer
-from django.http import Http404
-class ProductView(viewsets.ViewSet):
+from products.producer import publish
 
-     # api/product
-    def get_object(self,pk):
-        try:
-            return Product.objects.get(pk=pk)
-        except Product.DoesNotExist:
-            raise Http404
+# create product model or list of the products ( get, post)
+class ProductListCraeteAPIView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
 
-    def list(self,request):
-        products = Product.objects.all()
-        serialize = ProductSerializer(products, many=True)
-        return Response(serialize.data)
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = ProductSerializer(queryset, many=True)
+        publish()
+        return Response(serializer.data)
 
-    def create(self,request):
-        serializer = ProductSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+class ProductRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
 
-    def retrieve(self,request,pk=None):
-        product = self.get_object(pk=pk)
-        serialize = ProductSerializer(product)
-        return Response(serialize.data)
-                    
-    def destroy(self,request,pk=None):
-        product = self.get_object(pk=pk)
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
 
-    def update(self,request,pk=None):
-        product = self.get_object(pk=pk)
-        serializer = ProductSerializer(product, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.error, status=status.HTTP_204_NO_CONTENT)
-
-    
+class userAPIView(APIView):
+    def get(self, __):
+        user = User.objects.all()
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+        
