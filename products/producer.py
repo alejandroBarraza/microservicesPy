23b9a5197201a91312ddaa156b89de40104fe1ff
alejandroBarraza <alejandro.barraza@alumnos.ucn.cl>
2023-01-10 04:10:08 +0900

    modified channel rabbitmq

--- products/producer.py
+++ products/producer.py
@@ -9,5 +9,5 @@ channel = connection.channel()
 def publish(method, body):
 
     properties = pika.BasicProperties(method)
-    channel.basic_publish(exchange='', routing_key='main', body=json.dump(body), properties=properties)
+    channel.basic_publish(exchange='', routing_key='main', body=json.dumps(body), properties=properties)
 
